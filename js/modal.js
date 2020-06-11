117f487b864a0f51151b6330a372e78565a2375e
Brus Palaj <brusp@bruss-mbp.home>
2020-06-11 12:38:05 -0400

    more changes

--- js/modal.js
+++ js/modal.js
@@ -1,41 +1,40 @@
-const portfolioContainer = document.querySelector('.portfolio-items')
+const portfolioContainer = document.querySelector(".portfolio-items");
 
-portfolioContainer.addEventListener('click', e => {
+portfolioContainer.addEventListener("click", (e) => {
   // console.log(e)
-  e.preventDefault()
+  e.preventDefault();
 
-  const modalToggle = e.target.closest('.portfolio-link')
+  const modalToggle = e.target.closest(".portfolio-link");
   // console.log(modalToggle)
-  if (! modalToggle) return
+  if (!modalToggle) return;
 
-  const modal = modalToggle.parentNode.nextElementSibling
-  const closeButton = modal.querySelector('.modal-close')
+  const modal = modalToggle.parentNode.nextElementSibling;
+  const closeButton = modal.querySelector(".modal-close");
 
-  const modalOpen = _ => {
-    modal.classList.add('is-open')
-    modal.style.animation = 'modalIn 500ms forwards'
+  const modalOpen = (_) => {
+    modal.classList.add("is-open");
+    modal.style.animation = "modalIn 500ms forwards";
     //document.body.style.overflowY = 'hidden'
-  }
+  };
 
-  const modalClose = _ => {
-    modal.classList.remove('is-open')
-    modal.removeEventListener('animationend', modalClose)
-  }
+  const modalClose = (_) => {
+    modal.classList.remove("is-open");
+    modal.removeEventListener("animationend", modalClose);
+  };
 
-  closeButton.addEventListener('click', _ => {
-    modal.style.animation = 'modalOut 500ms forwards'
-    modal.addEventListener('animationend', modalClose)
+  closeButton.addEventListener("click", (_) => {
+    modal.style.animation = "modalOut 500ms forwards";
+    modal.addEventListener("animationend", modalClose);
     //document.body.style.overflowY = 'scroll'
-  })
+  });
 
-  document.addEventListener('keydown', e => {
-    if ( e.keyCode === 27 ) {
-      modal.style.animation = 'modalOut 500ms forwards'
-      modal.addEventListener('animationend', modalClose)
-      document.body.style.overflowY = 'scroll'
+  document.addEventListener("keydown", (e) => {
+    if (e.keyCode === 27) {
+      modal.style.animation = "modalOut 500ms forwards";
+      modal.addEventListener("animationend", modalClose);
+      document.body.style.overflowY = "scroll";
     }
-  })
+  });
 
-  modalOpen()
-
-})
+  modalOpen();
+});
